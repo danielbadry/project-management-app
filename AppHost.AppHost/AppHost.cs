f34dc3f031a2beac093691f44d365ef4c7f57074
daniel badri <danielbadry@gmail.com>
2026-06-01 12:31:13 +0200

    initial project

---
+++ AppHost.AppHost/AppHost.cs
@@ -0,0 +1,12 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.AppHost_ApiService>("apiservice")
+    .WithHttpHealthCheck("/health");
+
+builder.AddProject<Projects.AppHost_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health")
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
