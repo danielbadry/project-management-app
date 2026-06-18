679c7a43332a1495282404e750764ca66bab616e
daniel badri <danielbadry@gmail.com>
2026-06-18 11:08:47 +0200

    feat: separate logic from UI

---
+++ AppHost.Web/Components/Pages/Login.razor.cs
@@ -0,0 +1,29 @@
+﻿using Dtos = AppHost.Web.Dtos;
+using AppHost.Web.Services;
+using Microsoft.AspNetCore.Components;
+namespace AppHost.Web.Components.Pages;
+
+public partial class Login
+{
+    public Dtos.Login LoginDto { set; get; } = new();
+
+    [Inject]
+    private AuthService AuthService { get; set; } = null!;
+
+    [Inject]
+    private NavigationManager Navigation { get; set; } = null!;
+
+    public async Task HandleLogin()
+    {
+        var success =
+                    await AuthService.Login(
+                        LoginDto.Username,
+                        LoginDto.Password);
+
+        if (success)
+        {
+            Navigation.NavigateTo("/");
+        }
+    }
+
+}
